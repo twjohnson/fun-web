5ad5cf83fb5d38b9aefdf74e4b64e6a167d6d417
Izzet Mustafayev <izzet_mustafayev@epam.com>
2016-09-30 23:24:58 +0300

    Step 12: attache Elm app to container

--- secounter/web/static/js/app.js
+++ secounter/web/static/js/app.js
@@ -19,3 +19,6 @@ import "phoenix_html"
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
+// Set up Elm App
+const elmDiv = document.querySelector("#elm-container");
+const elmApp = Elm.SecounterApp.embed(elmDiv);
